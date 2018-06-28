602dc637ec01d9ba6bb88507d8710ae32eb6af2b
Phil Lachmann <phil.lachmann@ingagepartners.com>
2018-06-28 18:49:57 -0400

    track selections in cookie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CookieModule } from "ngx-cookie";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, CookieModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { States } from "../shared/constants";
+import { CookieService } from "ngx-cookie";
 
 @Component({
-  selector: 'lpg-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "lpg-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  states = [];
+  selectedStates = [];
 
-  constructor() { }
+  constructor(public cookieService: CookieService) {}
 
   ngOnInit() {
+    this.states = States;
+    this.selectedStates = this.getCookie("statelist");
   }
 
+  getCookie(key: string): any {
+    const a = this.cookieService.getObject(key);
+    console.log("getcookie");
+    console.log(a);
+    return a;
+  }
+
+  setCookie(key: string, value: string[]) {
+    this.cookieService.putObject(key, value);
+  }
+
+  listClick(event, newValue) {
+    console.log(newValue);
+    this.addorremoveState(newValue);
+
+    // don't forget to update the model here
+    // ... do other stuff here ...
+  }
+
+  private addorremoveState(state: string) {
+    if (this.isInSelectedList(state)) {
+      this.selectedStates = this.selectedStates.filter(item => item !== state);
+    } else {
+      this.selectedStates.push(state);
+    }
+    console.log(this.selectedStates);
+    this.setCookie("statelist", this.selectedStates);
+  }
+
+  public isInSelectedList(item: string): boolean {
+    const found = this.selectedStates.find(function(element) {
+      return element === item;
+    });
+
+    // console.log(found);
+
+    return found !== undefined;
+  }
 }
